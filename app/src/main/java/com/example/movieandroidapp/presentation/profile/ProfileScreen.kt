8583a823e8c1bbe682eaec06ee94817cb260db99
HrvojeCutvaric <hcutvaric@gmail.com>
2024-05-10 14:02:27 +0200

    Design login user profile screen.

--- app/src/main/java/com/example/movieandroidapp/presentation/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/movieandroidapp/presentation/profile/ProfileScreen.kt
@@ -1,23 +1,42 @@
 package com.example.movieandroidapp.presentation.profile
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
+import androidx.compose.material.icons.automirrored.filled.Logout
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -33,6 +52,8 @@ import com.example.movieandroidapp.domain.utils.Screen
 import com.example.movieandroidapp.presentation.core.signInScreen.SignInViewModel
 import com.example.movieandroidapp.presentation.core.signUpScreen.SignUpViewModel
 import com.example.movieandroidapp.presentation.theme.Background
+import com.example.movieandroidapp.presentation.theme.CardBackground
+import com.example.movieandroidapp.presentation.theme.IconColor
 import com.example.movieandroidapp.presentation.theme.Primary
 import com.example.movieandroidapp.presentation.theme.TextPrimary
 import com.example.movieandroidapp.presentation.theme.TextSecondary
@@ -118,27 +139,135 @@ fun ProfileScreen(mainNavController: NavHostController) {
     } else {
         Column(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Box(
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp)
+                    .clip(RoundedCornerShape(100.dp))
+                    .background(TextSecondary),
+                contentAlignment = Alignment.Center
+            ) {
+
+                Icon(
+                    modifier = Modifier.size(60.dp),
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = "Person Icon",
+                    tint = Color.Black
+                )
+
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
             Text(
-                text = signInViewModel.currentUser?.email ?: "",
-                fontSize = 24.sp,
-                fontWeight = FontWeight.SemiBold
-            )
-            Text(
-                modifier = Modifier.clickable {
-                    signInViewModel.logout()
-                    mainNavController.navigate(Screen.Main.rout)
-                },
-                text = stringResource(R.string.logout),
+                modifier = Modifier.fillMaxWidth(),
+                text = if (signInViewModel.currentUser?.displayName == "") stringResource(R.string.no_display_name) else signInViewModel.currentUser?.displayName
+                    ?: stringResource(R.string.no_display_name),
                 fontSize = 18.sp,
-                fontWeight = FontWeight.Medium,
-                color = Primary
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center
             )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = signInViewModel.currentUser?.email ?: "",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            val items = listOf(
+                ProfileOptionsItem(
+                    icon = Icons.Filled.Edit,
+                    name = stringResource(R.string.edit_profile),
+                    color = TextSecondary,
+                    showNextIcon = true,
+                    onClick = {}
+                ),
+                ProfileOptionsItem(
+                    icon = Icons.Filled.Lock,
+                    name = stringResource(R.string.change_password),
+                    color = TextSecondary,
+                    showNextIcon = true,
+                    onClick = {}
+                ),
+                ProfileOptionsItem(
+                    icon = Icons.Filled.Bookmark,
+                    name = stringResource(R.string.saved),
+                    color = TextSecondary,
+                    showNextIcon = true,
+                    onClick = {}
+                ),
+                ProfileOptionsItem(
+                    icon = Icons.AutoMirrored.Filled.Logout,
+                    name = stringResource(R.string.logout),
+                    color = Color.Red,
+                    showNextIcon = false,
+                    onClick = {}
+                ),
+            )
+
+            items.forEach { item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .drawBehind {
+
+                            val borderSize = 0.5.dp.toPx()
+
+                            drawLine(
+                                CardBackground,
+                                Offset(0f, 0f),
+                                Offset(size.width, 0f),
+                                borderSize
+                            )
+                        }
+                        .clickable {
+                            item.onClick()
+                        }
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .padding(start = 16.dp, top = 8.dp, bottom = 8.dp),
+                        imageVector = item.icon,
+                        contentDescription = item.name,
+                        tint = item.color
+                    )
+                    Text(
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(horizontal = 16.dp, vertical = 8.dp),
+                        text = item.name,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = item.color
+                    )
+
+                    if(item.showNextIcon) {
+                        Icon(
+                            modifier = Modifier
+                                .padding(end = 16.dp, top = 8.dp, bottom = 8.dp),
+                            imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
+                            contentDescription = stringResource(R.string.next_icon),
+                            tint = IconColor
+                        )
+                    }
+                }
+            }
+
         }
     }
 
-
 }
+
+data class ProfileOptionsItem(
+    val icon: ImageVector,
+    val name: String,
+    val color: Color,
+    val showNextIcon: Boolean,
+    val onClick: () -> Unit
+)
